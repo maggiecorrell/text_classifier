fd2c772fd5c3bfc0d7cfe6495cf694b5079d81b8
Jon Alligood <j.alligood47@gmail.com>
2016-09-02 12:00:46 -0400

    Started functional tests

--- classifier/urls.py
+++ classifier/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'$', views.index, name='index')
+    url(r'$', views.index, name='index'),
     url(r'^login/', views.signin, name='signin'),
     url(r'^logout/', views.signout, name='signout'),
     url(r'^register/', views.register, name='register'),
