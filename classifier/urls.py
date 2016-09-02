b934d20cb57c1b2cdc8aa7190d63c43d5fe03408
Maggie Correll <maggie.correll@gmail.com>
2016-09-02 12:02:43 -0400

    beginning of registration

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-    url(r'$', views.index, name='index')
-    url(r'^login/', views.signin, name='signin'),
-    url(r'^logout/', views.signout, name='signout'),
-    url(r'^register/', views.register, name='register'),
+    url(r'$', views.index, name='index'),
+    url(r'^register/$', CreateView.as_view(
+            template_name='registration/register.html',
+            form_class=UserCreationForm,
+            success_url='/'
+
+    ), name='register'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/login'}, name='logout'),
 ]
